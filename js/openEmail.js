5417efdb67a87198ba807bc18a1240408e1de7fb
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-28 10:42:03 -0300

    Initial commit Angular Portfólio.

--- js/openEmail.js
+++
@@ -1,8 +0,0 @@
-const email = document.getElementById("email");
-
-email.addEventListener("click", function(e){
-    e.preventDefault();
-    const textEmail = document.getElementById("box-email");textEmail.style.display="flex";
-})
-
-
